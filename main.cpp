2df3b7374e191193b3d07f6d7b5f94668427ad74
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2023-02-11 18:49:09 -0500

    created main function for testing

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "load_balancer.h"
+#include "server.h"
+
+int main()
+{
+    LoadBalancer lb(5);
+    std::vector<Server> servers;
+    for (int i = 0; i < 5; i++)
+    {
+        servers.emplace_back(i);
+    }
+    for (int i = 0; i < 20; i++)
+    {
+        int serverId = lb.getNextServer();
+        std::cout << "Request " << i << " sent to server " << serverId << std::endl;
+    }
+    return 0;
+}
